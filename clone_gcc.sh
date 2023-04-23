56f8ae1d56294a5b77321a77b5b23d9961836596
dexter <115158271+DexterNoob@users.noreply.github.com>
2023-04-23 07:13:23 +0700

    Update clone_gcc.sh

--- clone_gcc.sh
+++ clone_gcc.sh
@@ -7,5 +7,5 @@ if [ ! -d $GCC64 ]; then
     git clone --depth=1 https://github.com/DexterNoob/gcc-arm64 -b master $GCC64
 fi
 if [ ! -d $GCC ]; then
-    git clone --depth=1 https://github.com/DexterNoob/gcc-arm64 -b master $GCC
+    git clone --depth=1 https://github.com/DexterNoob/gcc-arm32 -b master $GCC
 fi
