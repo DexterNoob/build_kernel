563e32e3a16b6645e1be676dd3d4d67431fcccad
dexter <115158271+DexterNoob@users.noreply.github.com>
2023-04-23 07:45:01 +0700

    Update build_gcc.sh

--- build_gcc.sh
+++ build_gcc.sh
@@ -32,7 +32,7 @@ export KBUILD_BUILD_USER="Noobbeast"
 export KBUILD_BUILD_HOST="GithubServer"
 Defconfig="begonia_user_defconfig"
 KERNEL_NAME=$(cat "$MainPath/arch/arm64/configs/$Defconfig" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
-ZIP_KERNEL_VERSION="4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"
+ZIP_KERNEL_VERSION="v4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"
 TIME=$(date +"%m%d%H%M")
 
 # Start building
