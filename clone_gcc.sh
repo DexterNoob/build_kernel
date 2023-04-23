d070eeba8d2406616f71881de48b3ba9385576a8
dexter <115158271+DexterNoob@users.noreply.github.com>
2023-04-23 07:12:58 +0700

    Update clone_gcc.sh
    Update link gcc Toolchain

--- clone_gcc.sh
+++ clone_gcc.sh
@@ -4,8 +4,8 @@ GCC64="$(pwd)/GCC64"
 GCC="$(pwd)/GCC"
 
 if [ ! -d $GCC64 ]; then
-    git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 $GCC64
+    git clone --depth=1 https://github.com/DexterNoob/gcc-arm64 -b master $GCC64
 fi
 if [ ! -d $GCC ]; then
-    git clone --depth=1 https://github.com/mvaisakh/gcc-arm $GCC
+    git clone --depth=1 https://github.com/DexterNoob/gcc-arm64 -b master $GCC
 fi
