5cf468c6d4353f6451eabd529eeccdc9de6fcd45
dexter <115158271+DexterNoob@users.noreply.github.com>
2023-05-08 19:03:24 +0700

    build_dtc.sh

--- build_dtc.sh
+++ build_dtc.sh
@@ -21,7 +21,7 @@ MakeZip() {
     fi
     cp -af $MainPath/out/arch/arm64/boot/Image.gz-dtb $Any
     sed -i "s/kernel.string=.*/kernel.string=$KERNEL_NAME-$HeadCommit test by $KBUILD_BUILD_USER/g" anykernel.sh
-    zip -r9 $MainPath/"[$Compiler][R-OSS]-$ZIP_KERNEL_VERSION-$KERNEL_NAME-$TIME.zip" * -x .git README.md *placeholder
+    zip -r9 $MainPath/"$Compiler-$ZIP_KERNEL_VERSION-$KERNEL_NAME-$TIME.zip" * -x .git README.md *placeholder
     cd $MainPath
 }
 
@@ -29,15 +29,15 @@ MakeZip() {
 HeadCommit="$(git log --pretty=format:'%h' -1)"
 export ARCH="arm64"
 export SUBARCH="arm64"
-export KBUILD_BUILD_USER="TeraaBytee"
+export KBUILD_BUILD_USER="Mr.Kintil"
 export KBUILD_BUILD_HOST="GithubServer"
 Defconfig="begonia_user_defconfig"
 KERNEL_NAME=$(cat "$MainPath/arch/arm64/configs/$Defconfig" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
-ZIP_KERNEL_VERSION="4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"
+ZIP_KERNEL_VERSION="v4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"
 TIME=$(date +"%m%d%H%M")
 
 # Start building
-Compiler=DragonTC
+Compiler=DTC-R
 MAKE="./makeparallel"
 rm -rf out
 BUILD_START=$(date +"%s")
